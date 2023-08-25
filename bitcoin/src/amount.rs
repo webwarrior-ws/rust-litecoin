039797cef6a9542811fcdf24c1db87a30f35a955
Tobin C. Harding <me@tobin.cc>
2023-08-25 12:30:04 +1000

    Remove useless usage of vec! macro
    Clippy emits a bunch of warnings of form:
    
      warning: useless use of `vec!`
    
    As suggested, remove the vec and just use an array.

--- bitcoin/src/amount.rs
+++ bitcoin/src/amount.rs
@@ -2303,9 +2303,7 @@ mod tests {
 
     #[test]
     fn disallow_confusing_forms() {
-        // Non-exhaustive list of confusing forms.
-        let confusing =
-            vec!["Msat", "Msats", "MSAT", "MSATS", "MSat", "MSats", "MBTC", "Mbtc", "PBTC"];
+        let confusing = ["Msat", "Msats", "MSAT", "MSATS", "MSat", "MSats", "MBTC", "Mbtc", "PBTC"];
         for denom in confusing.iter() {
             match Denomination::from_str(denom) {
                 Ok(_) => panic!("from_str should error for {}", denom),
@@ -2318,7 +2316,7 @@ mod tests {
     #[test]
     fn disallow_unknown_denomination() {
         // Non-exhaustive list of unknown forms.
-        let unknown = vec!["NBTC", "UBTC", "ABC", "abc"];
+        let unknown = ["NBTC", "UBTC", "ABC", "abc"];
         for denom in unknown.iter() {
             match Denomination::from_str(denom) {
                 Ok(_) => panic!("from_str should error for {}", denom),
